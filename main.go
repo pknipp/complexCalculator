a5d052b3318786f74f3ac961dd3257f1f469ab1a
Peter Knipp <peteraknipp@gmail.com>
2021-11-09 10:46:13 -0500

    Copy "calculate" function (along w/math/cmplx import statement) into heroku's main.go

--- main.go
+++ main.go
@@ -4,10 +4,28 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"math/cmplex"
 
 	"github.com/gin-gonic/gin"
 	_ "github.com/heroku/x/hmetrics/onload"
 )
+
+func calculate(z1 complex128, op string, z2 complex128) complex128 {
+	var result complex128
+	switch op {
+	case "+":
+		result = z1 + z2
+	case "-":
+		result = z1 - z2
+	case "*":
+		result = z1 * z2
+	case "/":
+		result = z1 / z2
+	case "^":
+		result = cmplx.Pow(z1, z2)
+	}
+	return result
+}
 
 func main() {
 	port := os.Getenv("PORT")
