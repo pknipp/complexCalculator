f87f90f65a486ae4f5b1f909b89d7728a3f22a84
Peter Knipp <peteraknipp@gmail.com>
2021-11-09 10:52:16 -0500

    Copy "parseExpression" function (and requisite import lines) into heroku's main.go

--- main.go
+++ main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"log"
+	"math/cmplx"
 	"net/http"
 	"os"
-	"math/cmplx"
+	"strconv"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 	_ "github.com/heroku/x/hmetrics/onload"
@@ -25,6 +27,103 @@ func calculate(z1 complex128, op string, z2 complex128) complex128 {
 		result = cmplx.Pow(z1, z2)
 	}
 	return result
+}
+
+func parseExpression (expression string) (complex128) {
+	// m1 := regexp.MustCompile(`\*\*`)
+	// expression = m1.ReplaceAllString(expression, "^")
+	expression = regexp.MustCompile(`\*\*`).ReplaceAllString(expression, "^")
+	getNumber := func(expression string) (complex128, string){
+		leadingChar := expression[0:1]
+		if leadingChar == "(" {
+			nExpression := 0
+			nParen := 1
+			for nParen > 0 {
+				nExpression++
+				nextChar := expression[nExpression: nExpression + 1]
+				if nextChar == "(" {
+					nParen++
+				} else if nextChar == ")" {
+					nParen--
+				}
+			}
+			return parseExpression(expression[1: nExpression]), expression[nExpression + 1:]
+		} else if leadingChar == "i" || leadingChar == "j" {
+			return complex(0, 1), expression[1:]
+		} else {
+			p := 1
+			var lastNum complex128
+			for len(expression) >= p {
+				z := expression[0:p]
+				// If implied multiplication is detected ...
+				if expression[p - 1: p] == "(" {
+					// ... insert a "*" symbol.
+					expression = expression[0:p - 1] + "*" + expression[p - 1:]
+					break
+				} else if !(z == "." || z == "-" || z == "-.") {
+					num, err := strconv.ParseFloat(z, 64)
+					if err != nil {
+						break
+					}
+					lastNum = complex(num, 0.)
+				}
+				p++
+			}
+			return lastNum, expression[p - 1:]
+		}
+	}
+	type opNum struct {
+		op string
+		num complex128
+	}
+
+	expression = strings.ReplaceAll(expression, " ", "")
+	if expression[0:1] == "+" {
+		expression = expression[1:]
+	}
+	var z complex128
+	z, expression = getNumber(expression)
+	precedence := map[string]int{"+": 0, "-": 0, "*": 1, "/": 1, "^": 2}
+	ops := "+-*/^"
+	pairs := []opNum{}
+	var num complex128
+	for len(expression) > 0 {
+		op := expression[0:1]
+		if strings.Contains(ops, op) {
+			expression = expression[1:]
+		} else {
+			op = "*"
+		}
+		num, expression = getNumber(expression)
+		pair := opNum{op, num}
+		pairs = append(pairs, pair)
+	}
+	for len(pairs) > 0 {
+		index := 0
+		for len(pairs) > index {
+			if index < len(pairs) - 1 && precedence[pairs[index].op] < precedence[pairs[index + 1].op] {
+				index++
+			} else {
+				var z1 complex128
+				if index == 0 {
+					z1 = z
+				} else {
+					z1 = pairs[index - 1].num
+				}
+				result := calculate(z1, pairs[index].op, pairs[index].num)
+				if index == 0 {
+					z = result
+					pairs = pairs[1:]
+				} else {
+					pairs[index - 1].num = result
+					pairs = append(pairs[0: index], pairs[index + 1:]...)
+				}
+				// Start another loop thru the expression, ISO high-precedence operations.
+				index = 0
+			}
+		}
+	}
+	return z
 }
 
 func main() {
