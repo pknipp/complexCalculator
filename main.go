3c73d4752962939bb91162bcccf6e1e2d243fb3b
Peter Knipp <peteraknipp@gmail.com>
2021-11-15 12:22:19 -0500

    Minor changes

--- main.go
+++ main.go
@@ -1,17 +1,19 @@
 package main
 
 import (
-	// "fmt"
+	"fmt"
 	// "io"
-	"log"
+	// "log"
 	"math/cmplx"
-	"net/http"
-	"os"
+	// "net/http"
+	// "os"
 	"regexp"
 	"strconv"
-	"strings"
-	"github.com/gin-gonic/gin"
-	_ "github.com/heroku/x/hmetrics/onload"
+	// "strings"
+	// "github.com/gin-gonic/gin"
+	// _ "github.com/heroku/x/hmetrics/onload"
+
+	"pknipp/parseExpression"
 )
 
 
@@ -208,156 +210,156 @@ func doRegExp(expression string) string {
 	return expression
 }
 
-func parseExpression (expression string) (string, complex128) {
-	ZERO := complex(0., 0.)
-	message := ""
-	// Following pre-processing line is needed if/when this code is tested in a non-server configuration.
-	expression = doRegExp(expression)
-	getNumber := func(expression string) (string, complex128, string){
-		// ZERO := complex(0., 0.)
-		message := ""
-		var val complex128
-		if len(expression) == 0 {
-			return "Your expression truncates prematurely.", val, expression
-		}
-		leadingChar := expression[0:1]
-		if leadingChar == "(" {
-			var nExpression int
-			// remove leading parenthesis
-			expression = expression[1:]
-			message, nExpression = findSize(expression)
-			if len(message) != 0 {
-				return message, ZERO, ""
-			}
-			// recursive call to evalulate what is in parentheses
-			message, val = parseExpression(expression[0:nExpression])
-			if len(message) != 0 {
-				return message, ZERO, ""
-			}
-			// From expression remove trailing parenthesis and stuff preceding it.
-			expression = expression[nExpression + 1:]
-			return message, val, expression
-		} else if leadingChar == "i" {
-			return message, complex(0, 1), expression[1:]
-			// A capital letter triggers that we are looking at start of a unary function name.
-		} else if strings.Contains("ABCDEFGHIJKLMNOPQRSTUVWXYZ", leadingChar) {
-			method := leadingChar
-			expression = expression[1:]
-			if len(expression) == 0 {
-				return "This unary function invocation ends prematurely.", 0, expression
-			}
-			// We seek an open paren, which signifies start of argument (& end of method name)
-			for expression[0:1] != "(" {
-				method += expression[0: 1]
-				expression = expression[1:]
-				if len(expression) == 0 {
-					return "The argument of this unary function seems nonexistent.", 0, expression
-				}
-			}
-			var nExpression int
-			// Remove leading parenthesis
-			expression = expression[1:]
-			message, nExpression = findSize(expression)
-			var arg complex128
-			if len(message) != 0 {
-				return message, ZERO, ""
-			}
-			message, arg = parseExpression(expression[0: nExpression])
-			if len(message) != 0 {
-				return message, ZERO, ""
-			}
-			message, val = unary(method, arg)
-			return message, val, expression[nExpression + 1:]
-		} else {
-			// The following'll change only if strconv.ParseFloat ever returns no error, below.
-			message = "The string '" + expression + "' does not evaluate to a number."
-			p := 1
-			for len(expression) >= p {
-				z := expression[0:p]
-				// If implied multiplication is detected ...
-				if expression[p - 1: p] == "(" {
-					// ... insert a "*" symbol.
-					expression = expression[0:p - 1] + "*" + expression[p - 1:]
-					break
-				} else if !(z == "." || z == "-" || z == "-.") {
-					num, err := strconv.ParseFloat(z, 64)
-					if err != nil {
-						break
-					}
-					val = complex(num, 0.)
-					message = ""
-				}
-				p++
-			}
-			return message, val, expression[p - 1:]
-		}
-	}
-	type opNum struct {
-		op string
-		num complex128
-	}
-	if len(expression) > 0 {
-		if expression[0:1] == "+" {
-			expression = expression[1:]
-		}
-	}
-	var z complex128
-	message, z, expression = getNumber(expression)
-	if len(message) != 0 {
-		return message, ZERO
-	}
-	PRECEDENCE := map[string]int{"+": 0, "-": 0, "*": 1, "/": 1, "^": 2}
-	OPS := "+-*/^"
-	pairs := []opNum{}
-	var num complex128
-	for len(expression) > 0 {
-		op := expression[0:1]
-		if strings.Contains(OPS, op) {
-			expression = expression[1:]
-		} else {
-			op = "*"
-		}
-		message, num, expression = getNumber(expression)
-		if len(message) != 0 {
-			return message, ZERO
-		}
-		pairs = append(pairs, opNum{op, num})
-	}
-	for len(pairs) > 0 {
-		index := 0
-		for len(pairs) > index {
-			if index < len(pairs) - 1 && PRECEDENCE[pairs[index].op] < PRECEDENCE[pairs[index + 1].op] {
-				index++
-			} else {
-				var z1 complex128
-				if index == 0 {
-					z1 = z
-				} else {
-					z1 = pairs[index - 1].num
-				}
-				var result complex128
-				message, result = binary(z1, pairs[index].op, pairs[index].num)
-				if index == 0 {
-					z = result
-					pairs = pairs[1:]
-				} else {
-					pairs[index - 1].num = result
-					pairs = append(pairs[0: index], pairs[index + 1:]...)
-				}
-				// Start another loop thru the expression, ISO high-precedence operations.
-				index = 0
-			}
-		}
-	}
-	return message, z
-}
+// func parseExpression (expression string) (string, complex128) {
+// 	ZERO := complex(0., 0.)
+// 	message := ""
+// 	// Following pre-processing line is needed if/when this code is tested in a non-server configuration.
+// 	expression = doRegExp(expression)
+// 	getNumber := func(expression string) (string, complex128, string){
+// 		// ZERO := complex(0., 0.)
+// 		message := ""
+// 		var val complex128
+// 		if len(expression) == 0 {
+// 			return "Your expression truncates prematurely.", val, expression
+// 		}
+// 		leadingChar := expression[0:1]
+// 		if leadingChar == "(" {
+// 			var nExpression int
+// 			// remove leading parenthesis
+// 			expression = expression[1:]
+// 			message, nExpression = findSize(expression)
+// 			if len(message) != 0 {
+// 				return message, ZERO, ""
+// 			}
+// 			// recursive call to evalulate what is in parentheses
+// 			message, val = parseExpression(expression[0:nExpression])
+// 			if len(message) != 0 {
+// 				return message, ZERO, ""
+// 			}
+// 			// From expression remove trailing parenthesis and stuff preceding it.
+// 			expression = expression[nExpression + 1:]
+// 			return message, val, expression
+// 		} else if leadingChar == "i" {
+// 			return message, complex(0, 1), expression[1:]
+// 			// A capital letter triggers that we are looking at start of a unary function name.
+// 		} else if strings.Contains("ABCDEFGHIJKLMNOPQRSTUVWXYZ", leadingChar) {
+// 			method := leadingChar
+// 			expression = expression[1:]
+// 			if len(expression) == 0 {
+// 				return "This unary function invocation ends prematurely.", 0, expression
+// 			}
+// 			// We seek an open paren, which signifies start of argument (& end of method name)
+// 			for expression[0:1] != "(" {
+// 				method += expression[0: 1]
+// 				expression = expression[1:]
+// 				if len(expression) == 0 {
+// 					return "The argument of this unary function seems nonexistent.", 0, expression
+// 				}
+// 			}
+// 			var nExpression int
+// 			// Remove leading parenthesis
+// 			expression = expression[1:]
+// 			message, nExpression = findSize(expression)
+// 			var arg complex128
+// 			if len(message) != 0 {
+// 				return message, ZERO, ""
+// 			}
+// 			message, arg = parseExpression(expression[0: nExpression])
+// 			if len(message) != 0 {
+// 				return message, ZERO, ""
+// 			}
+// 			message, val = unary(method, arg)
+// 			return message, val, expression[nExpression + 1:]
+// 		} else {
+// 			// The following'll change only if strconv.ParseFloat ever returns no error, below.
+// 			message = "The string '" + expression + "' does not evaluate to a number."
+// 			p := 1
+// 			for len(expression) >= p {
+// 				z := expression[0:p]
+// 				// If implied multiplication is detected ...
+// 				if expression[p - 1: p] == "(" {
+// 					// ... insert a "*" symbol.
+// 					expression = expression[0:p - 1] + "*" + expression[p - 1:]
+// 					break
+// 				} else if !(z == "." || z == "-" || z == "-.") {
+// 					num, err := strconv.ParseFloat(z, 64)
+// 					if err != nil {
+// 						break
+// 					}
+// 					val = complex(num, 0.)
+// 					message = ""
+// 				}
+// 				p++
+// 			}
+// 			return message, val, expression[p - 1:]
+// 		}
+// 	}
+// 	type opNum struct {
+// 		op string
+// 		num complex128
+// 	}
+// 	if len(expression) > 0 {
+// 		if expression[0:1] == "+" {
+// 			expression = expression[1:]
+// 		}
+// 	}
+// 	var z complex128
+// 	message, z, expression = getNumber(expression)
+// 	if len(message) != 0 {
+// 		return message, ZERO
+// 	}
+// 	PRECEDENCE := map[string]int{"+": 0, "-": 0, "*": 1, "/": 1, "^": 2}
+// 	OPS := "+-*/^"
+// 	pairs := []opNum{}
+// 	var num complex128
+// 	for len(expression) > 0 {
+// 		op := expression[0:1]
+// 		if strings.Contains(OPS, op) {
+// 			expression = expression[1:]
+// 		} else {
+// 			op = "*"
+// 		}
+// 		message, num, expression = getNumber(expression)
+// 		if len(message) != 0 {
+// 			return message, ZERO
+// 		}
+// 		pairs = append(pairs, opNum{op, num})
+// 	}
+// 	for len(pairs) > 0 {
+// 		index := 0
+// 		for len(pairs) > index {
+// 			if index < len(pairs) - 1 && PRECEDENCE[pairs[index].op] < PRECEDENCE[pairs[index + 1].op] {
+// 				index++
+// 			} else {
+// 				var z1 complex128
+// 				if index == 0 {
+// 					z1 = z
+// 				} else {
+// 					z1 = pairs[index - 1].num
+// 				}
+// 				var result complex128
+// 				message, result = binary(z1, pairs[index].op, pairs[index].num)
+// 				if index == 0 {
+// 					z = result
+// 					pairs = pairs[1:]
+// 				} else {
+// 					pairs[index - 1].num = result
+// 					pairs = append(pairs[0: index], pairs[index + 1:]...)
+// 				}
+// 				// Start another loop thru the expression, ISO high-precedence operations.
+// 				index = 0
+// 			}
+// 		}
+// 	}
+// 	return message, z
+// }
 
 func handler(expression string) string {
 	// expression = expression[1:] This was used when I used r.URL.path
 	var message string
 	var result complex128
 	var resultString string
-	message, result = parseExpression(expression)
+	message, result = helper.parseExpression(expression)
 	if len(message) != 0 {
 		return "ERROR: " + message
 	}
@@ -407,42 +409,42 @@ func handler(expression string) string {
 // }
 
 func main() {
-	port := os.Getenv("PORT")
-	if port == "" {
-		log.Fatal("$PORT must be set")
-	}
+	// port := os.Getenv("PORT")
+	// if port == "" {
+		// log.Fatal("$PORT must be set")
+	// }
 	// I opted not to use this version of router, for technical reasons.
 	// router := gin.New()
-	router := gin.Default()
-	router.Use(gin.Logger())
-	router.LoadHTMLGlob("templates/*.tmpl.html")
-	router.Static("/static", "static")
-	router.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.tmpl.html", nil)
-	})
-	expressionText := "your expression"
-	resultText := "numerical value"
-	router.GET("/:expression", func(c *gin.Context) {
-		expression := doRegExp(c.Param("expression"))
-		c.HTML(http.StatusOK, "result.tmpl.html", gin.H{
-				"expressionText": expressionText,
-				"expressionValue": expression,
-				"resultText": resultText,
-				"resultValue": handler(expression),
-		})
-	})
-	router.GET("/json/:expression", func(c *gin.Context) {
-		expression := doRegExp(c.Param("expression"))
-		resultString := "{\"" + expressionText + "\": " + expression + ", \"" + resultText + "\": " + handler(expression) + "}"
-		c.String(http.StatusOK, resultString)
-	})
-	router.Run(":" + port)
+	// router := gin.Default()
+	// router.Use(gin.Logger())
+	// router.LoadHTMLGlob("templates/*.tmpl.html")
+	// router.Static("/static", "static")
+	// router.GET("/", func(c *gin.Context) {
+		// c.HTML(http.StatusOK, "index.tmpl.html", nil)
+	// })
+	// expressionText := "your expression"
+	// resultText := "numerical value"
+	// router.GET("/:expression", func(c *gin.Context) {
+		// expression := doRegExp(c.Param("expression"))
+		// c.HTML(http.StatusOK, "result.tmpl.html", gin.H{
+				// "expressionText": expressionText,
+				// "expressionValue": expression,
+				// "resultText": resultText,
+				// "resultValue": handler(expression),
+		// })
+	// })
+	// router.GET("/json/:expression", func(c *gin.Context) {
+		// expression := doRegExp(c.Param("expression"))
+		// resultString := "{\"" + expressionText + "\": " + expression + ", \"" + resultText + "\": " + handler(expression) + "}"
+		// c.String(http.StatusOK, resultString)
+	// })
+	// router.Run(":" + port)
 	// Use the following when testing the app in a non-server configuration.
-	// expression := "(1+2id(3-4id(5+6i)))**i"
-	// message, resultString := parseExpression(expression)
-	// if len(message) == 0 {
-		// fmt.Println(resultString)
-	// } else {
-		// fmt.Println(message)
-	// }
+	expression := "(1+2id(3-4id(5+6i)))**i"
+	message, resultString := helper.parseExpression(expression)
+	if len(message) == 0 {
+		fmt.Println(resultString)
+	} else {
+		fmt.Println(message)
+	}
 }
