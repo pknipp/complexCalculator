4973758cdd29d8a5b5c7698b035fb845bd3d009d
Peter Knipp <peteraknipp@gmail.com>
2021-12-13 14:08:38 -0500

    Add a "catch-all" router.get at the end, whose intent will be to catch people who use "/" instead of "d".

--- main.go
+++ main.go
@@ -454,6 +454,9 @@ func main() {
 		resultString := "{\"" + expressionText + "\": " + expression + ", \"" + resultText + "\": " + handler(expression) + "}"
 		c.String(http.StatusOK, resultString)
 	})
+	router.GET("/:expression", func(c *gin.Context) {
+		c.String(http.StatusOK, c.Param("expression"))
+	})
 	router.Run(":" + port)
 	// Use the following when testing the app in a non-server configuration.
 	// expression := "(1+2id(3-4id(5+6i)))**i"
